ca2cba4ab7429ae15103abaf0ccace9f978a4a03
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-03 00:45:05 +0530

    wip: page structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,24 @@
 function About() {
-  return <div>about</div>;
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A react app to search Github profiles and see profile details. This
+        project is part of the{' '}
+        <a href="https://www.udemy.com/course/react-front-to-back-2022/">
+          React Front To Back
+        </a>{' '}
+        Udemy Course by{' '}
+        <strong>
+          <a href="https://traversymedia.com"> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-6xl">Welcome</h1>
+    </div>
+  );
 }
 
 export default Home;
