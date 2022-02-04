827f245da160b5e8e07767f48f438f0c49f2ea69
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-04 23:55:29 +0530

    feat: add UserList Component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -13,10 +16,21 @@ function UserResults() {
     });
 
     const data = await response.json();
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
 
-  return <div>User results</div>;
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <h3>Loading...</h3>;
+  }
 }
 
 export default UserResults;
