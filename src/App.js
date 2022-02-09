38d4d2013c6ab0354de697505b14073dc57936bb
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-09 13:13:53 +0530

    feat: add User Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import User from './pages/User';
 import Alert from './components/layout/Alert';
 import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
                 <Route path="/notfound" element={<NotFound />} />
                 <Route path="/*" element={<NotFound />} />
               </Routes>
