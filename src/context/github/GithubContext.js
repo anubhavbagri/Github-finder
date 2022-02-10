68352329476263a9f6e240748b54fbe2f3a32884
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-10 08:08:10 +0530

    feat: add getUserRepos functionality

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {}, // empty object by default
+    repos: [],
     loading: false,
   };
 
@@ -59,6 +60,24 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  // Get User repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users?${login}/repos`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    });
+  };
+
   // Clear users from state
   const clearUsers = () => {
     dispatch({
@@ -79,9 +98,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
