5d616c042f2355cc15032116b2d137e81891ee4a
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-02-11 22:41:30 +0530

    refactor: pass whole state directly

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -103,10 +103,7 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
+        ...state,
         searchUsers,
         clearUsers,
         getUser,
